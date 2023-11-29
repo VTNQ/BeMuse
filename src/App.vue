6eea610d2ae66c27169d687958480c8336ec4ac0
Vo Tan Nam Quoc <115608795+VTNQ@users.noreply.github.com>
2023-11-29 18:56:58 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
+      <h1 class="font-bold">hello</h1>
     </div>
   </header>
 
