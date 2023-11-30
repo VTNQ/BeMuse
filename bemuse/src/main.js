f7770bd74d7c47c9d121cb9d9ac225423ae0c646
Kyos22 <nguyenthanhcongvt123@gmail.com>
2023-11-30 12:30:58 +0700

    up

--- bemuse/src/main.js
+++ bemuse/src/main.js
@@ -3,10 +3,14 @@ import './style.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+
 import router from './router'
 
 const app = createApp(App)
 
+
 app.use(router)
 
+
 app.mount('#app')
+
