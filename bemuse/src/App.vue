4ac81d535c7be0e554b4988cae2811b8bc9a69eb
tranp6648 <tranp6648@gmail.com>
2023-11-30 12:39:15 +0700

    update

--- bemuse/src/App.vue
+++ bemuse/src/App.vue
@@ -4,19 +4,9 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-<div class="h-[100vh] py-[45px] px-[100px]">
+<div class="h-[100vh] py-[45px] px-[100px] bg-black">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Homiiii</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+</div>
 </template>
 
 <style scoped>
