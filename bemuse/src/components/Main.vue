e82221f2d07b7c9f6836473b9435a6683267a9b3
Vo Tan Nam Quoc <115608795+VTNQ@users.noreply.github.com>
2023-12-02 09:53:53 +0700

    update

--- bemuse/src/components/Main.vue
+++ bemuse/src/components/Main.vue
@@ -2,10 +2,12 @@
 import MainChild1 from './MainChildrent/MainChild1.vue'
 import MainChild2 from './MainChildrent/MainChild2.vue';
 import MainChild5 from "./MainChildrent/MainChild5.vue";
+import MainChild3 from "@/components/MainChildrent/MainChild3.vue";
 
 
 export default {
   components: {
+    MainChild3,
     MainChild5,
     MainChild1,
     MainChild2,
@@ -24,10 +26,28 @@ export default {
         <MainChild2/>
       </div>
     </div>
+    <div class="h-full">
+      <MainChild3/>
+    </div>
+    <div class="h-full">
+      <MainChild4/>
+    </div>
     <div class="h-full relative max-w-full justify-center bg-amber-400 z-30">
       <div class="absolute w-full h-full bg-amber-400">
         <MainChild5/>
       </div>
+    </div>
+    <div class="h-full">
+      <MainChild6/>
+    </div>
+    <div class="h-full">
+      <MainChild7/>
+    </div>
+    <div class="h-full">
+      <MainChild8/>
+    </div>
+    <div class="h-full">
+      <MainChild9/>
     </div>
   </div>
 </template>
