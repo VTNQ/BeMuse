9fc4624bafe34eba7ee9ab3362710e51333e576b
Vo Tan Nam Quoc <115608795+VTNQ@users.noreply.github.com>
2023-12-02 15:25:47 +0700

    update

--- bemuse/src/components/MainChildrent/MainChild5.vue
+++ bemuse/src/components/MainChildrent/MainChild5.vue
@@ -4,11 +4,59 @@
 
 <template>
 <div class="max-w-full max-h-full relative absolute">
-  <div class="grid md:grid-cols-2 xl:grid-cols-4 justify-items-center p-16">
-    <div class="xl:max-w-full max-h-full justify-items-center mr-auto "><a href=""><img src="src/assets/images/5-1.jpg" alt="5-1.jpg" class="w-full h-full"></a></div>
-    <div class="xl:max-w-full max-h-full justify-items-center mx-auto "><a href=""><img src="src/assets/images/5-2.jpg" alt="5-2.jpg" class="w-full h-full"></a></div>
-    <div class="xl:max-w-full max-h-full justify-items-center mx-auto "><a href=""><img src="src/assets/images/5-3.jpg" alt="5-3.jpg" class="w-full h-full"></a></div>
-    <div class="xl:max-w-full max-h-full justify-items-center ml-auto"><a href=""><img src="src/assets/images/5-4.jpg" alt="5-4.jpg" class="w-full h-full"></a></div>
+  <div class="flex flex-wrap justify-around justify-start p-3 text-black">
+    <div class="xl:w-1/5 md:w-2/5 sm:max-w-full max-h-full ">
+      <div class="w-full relative">
+        <img src="src/assets/images/5-1.jpg" alt="5-1.jpg">
+        <a href="" class="absolute top-0 bottom-0 left-0 right-0 z-0"></a>
+      </div>
+      <div class="mt-3">
+        <time datetime="2023-10-12" class="font-light">Nov 12</time>
+      </div>
+      <a href="" class="mt-0">
+        <h1 class="font-bold text-2xl">Feminism In Art</h1>
+      </a>
+      <p class="font-light text-sm mr-10 flex-wrap">The Feminist art movement emerged in the late 1960s amidst the fervor of anti-war…</p>
+    </div>
+    <div class="xl:w-1/5 md:w-2/5 sm:max-w-full max-h-full ">
+      <div class="w-full relative">
+        <img src="src/assets/images/5-2.jpg" alt="5-2.jpg">
+        <a href="" class="absolute top-0 bottom-0 left-0 right-0 z-0"></a>
+      </div>
+      <div class="mt-3">
+        <time datetime="2023-10-12" class="font-light">Nov 12</time>
+      </div>
+      <a href="" class="mt-0">
+        <h1 class="font-bold text-2xl">Art Is For Everyone</h1>
+      </a>
+      <p class="font-light text-sm mr-10 flex-wrap">Activities like painting, sculpting, drawing, and photography can lower stress levels – providing a distraction from …</p>
+    </div>
+    <div class="xl:w-1/5 md:w-2/5 sm:max-w-full max-h-full ">
+      <div class="w-full relative">
+        <img src="src/assets/images/5-3.jpg" alt="5-3.jpg">
+        <a href="" class="absolute top-0 bottom-0 left-0 right-0 z-0"></a>
+      </div>
+      <div class="mt-3">
+        <time datetime="2023-10-12" class="font-light">Nov 12</time>
+      </div>
+      <a href="" class="mt-0">
+        <h1 class="font-bold text-2xl">How Can Art Change?</h1>
+      </a>
+      <p class="font-light text-sm mr-10 flex-wrap">Whatever your opinion, one thing is for sure: these little buddies are everywhere right now…</p>
+    </div>
+    <div class="xl:w-1/5 md:w-2/5 sm:max-w-full max-h-full ">
+      <div class="w-full relative">
+        <img src="src/assets/images/5-4.jpg" alt="5-4.jpg" >
+        <a href="" class="absolute top-0 bottom-0 left-0 right-0 z-0"></a>
+      </div>
+      <div class="mt-3">
+        <time datetime="2023-10-12" class="font-light">Nov 12</time>
+      </div>
+      <a href="" class="mt-0">
+        <h1 class="font-bold text-2xl">How Can Person Change?</h1>
+      </a>
+      <p class="font-light text-sm mr-10 flex-wrap">In terms of destroying art itself, one might think of Gustave Courbet’s successful…</p>
+    </div>
   </div>
 </div>
 </template>
