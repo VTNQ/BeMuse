2b236345cf70c4e4f311b0f5de6fd2216b147556
tranp6648 <tranp6648@gmail.com>
2023-12-04 01:06:20 +0700

    update

--- bemuse/src/components/Main.vue
+++ bemuse/src/components/Main.vue
@@ -47,7 +47,7 @@ export default {
     <div class="">
       <MainChild1 />
     </div>
-    <div  class="relative  bg-white   z-30 h-[36%] px-25  nav">
+    <div  class="relative  bg-white   z-30 h-[40%] px-25  nav" style="transition:all ease 3s">
       <div class="absolute flex justify-start items-center  w-full bg-white z-30">
   
         <MainChild2/>
