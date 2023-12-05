aca30a45fe007cbda71a96ba32d59ae70a8b5dbd
tranp6648 <tranp6648@gmail.com>
2023-12-05 18:18:45 +0700

    update

--- bemuse/src/App.vue
+++ bemuse/src/App.vue
@@ -2,29 +2,34 @@
 import { RouterLink, RouterView } from 'vue-router'
 import navbar from './components/navbar.vue'
 import Main from './components/Main.vue'
-
+import blog from './components/blog/Blog.vue'
 </script>
 
 <template>
-  <div class="flex">
+  <div
+   :style="{
+      display: $route.path.startsWith('/user') ? 'block' : '',
+      height: $route.path.startsWith('/user') ? '100%' : '',
+      width: $route.path.startsWith('/user') ? '100vw' : ''
+    }"
+    :class="{ flex: $route.path.startsWith('/user') }"
+  >
     
-    <!-- Navbar ở đây (nếu có chiều rộng cố định, hãy thêm vào) -->
-    <navbar class="xl:w-[23%] fixed top-0 left-0 z-50" /> <!-- Giả sử navbar chiếm 30% chiều rộng -->
+    <!-- Navbar ở đây (znếu có chiều rộng cố định, hãy thêm vào) -->
+    <navbar v-if="!$route.path.startsWith('/user')" class="xl:w-[23%] fixed top-0 left-0 z-50" /> <!-- Giả sử navbar chiếm 30% chiều rộng -->
     
     <!-- Main chiếm 70% chiều rộng và đặt ở bên phải -->
-    <Main class="xl:w-[77%] ml-auto h-full" />
+    <Main v-if="!$route.path.startsWith('/user')" class="xl:w-[77%] ml-auto h-full" />
     
-    
-    
+  
+  <blog v-if="$route.path.startsWith('/user')"/>
   </div>
 </template>
 
 
 
 <style scoped>
-.flex{
-  width: 100vw;
-}
+
 </style>
 <script>
 export default {
